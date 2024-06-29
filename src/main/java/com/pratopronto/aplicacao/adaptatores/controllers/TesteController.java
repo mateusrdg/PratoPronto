99646db3337a0c070e63be701ec3524e3e6bd130
matralves <malves@sauter.digital>
2024-06-29 20:40:43 -0300

    teste

--- src/main/java/com/pratopronto/aplicacao/adaptatores/controllers/TesteController.java
+++ src/main/java/com/pratopronto/aplicacao/adaptatores/controllers/TesteController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("teste")
 public class TesteController {
 
     private final ClienteServicePort clienteServicePort;
